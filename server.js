7e41c21af8d4cb314c0705aa4b17b8525fd2e24b
Simon Van de Water <simon.vandewater@gmail.com>
2018-02-19 15:51:41 +0100

    added web sockets to server

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 
 wss.on('connection', function connection(ws) {
+    console.log("Connected new client: %s!", ws);
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
     });
 
     ws.send('something');
 });
-
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
