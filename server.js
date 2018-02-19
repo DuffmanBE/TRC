62adbc88b4f4b5438922b1bc0ca056f0af9cdaf0
Simon Van de Water <simon.vandewater@gmail.com>
2018-02-19 15:37:01 +0100

    adding Websockets to server + client lib rx

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express()
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080});
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+    });
+
+    ws.send('something');
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
