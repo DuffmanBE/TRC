29e347942fc5db617ebd73c62d117a2a83d0959c
eljenso <jens.nicolay@gmail.com>
2018-02-22 06:45:49 +0100

    Ha! client -> server -> client

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
+const fs = require('fs');
 const express = require('express')
 const app = express()
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080});
 
+
+eval(String(fs.readFileSync("rx.all.js")));
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 
+const users = exports.Observable.interval(100);
+
+function handle(ws, x)
+{
+    console.log("handle: " + x);
+    const o = JSON.parse(x);
+    const cmd = o.cmd;
+    const obs = eval(cmd);
+    obs.subscribe(function (x)
+    {
+        console.log("sending " + x);
+        ws.send(JSON.stringify({x}));
+    })
+}
+
 wss.on('connection', function connection(ws) {
     console.log("Connected new client: %s!", ws);
-    ws.on('message', function incoming(message) {
-        console.log('received: %s', message);
-    });
+    ws.on('message', msg => handle(ws, msg));
 
-    ws.send('something');
 });
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
